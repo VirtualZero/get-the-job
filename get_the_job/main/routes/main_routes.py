68618cf4a6f550bb7527f1d77f0147615a6cc452
Birtchum <birtchum@virtualzero.net>
2019-03-16 10:00:14 -0400

    Finished contact form

--- get_the_job/main/routes/main_routes.py
+++ get_the_job/main/routes/main_routes.py
@@ -1,10 +1,61 @@
 from get_the_job import app
-from flask import render_template
+from get_the_job.forms.forms import ContactForm
+from flask import render_template, jsonify
+import os
+import requests
 
 
 @app.route('/')
 def landing():
+    contact_form = ContactForm()
     return render_template(
         'main/landing.html',
-        title="Portfolio"
+        title="Portfolio",
+        contact_form=contact_form
     )
+
+
+@app.route('/submit-message', methods=['POST'])
+def submit_message():
+    contact_form = ContactForm()
+
+    if contact_form.validate_on_submit():
+        send_message = requests.post(
+            'https://mailapi.virtualzero.tech/mail/send-mail',
+            headers={
+                'X-API-KEY': os.environ['MAIL_API_KEY']
+            },
+            json={
+                "recipients": ["birtchum@virtualzero.net"],
+                "subject": "New message from Portfolio",
+                "message": make_message(contact_form)
+            }
+        )
+
+        return jsonify(
+            {
+                'status': 'success'
+            }
+        ), 200
+
+    else:
+        errors = {}
+
+        for fieldName, errorMessages in contact_form.errors.items():
+            errors[fieldName] = errorMessages[0]
+
+        return jsonify(
+            {
+                'status': 'error',
+                'errors': errors
+            }
+        ), 400
+
+
+def make_message(contact_form):
+    return f"Here are the details of the message:\n" \
+           f"First Name: {contact_form.contact_first_name.data}\n" \
+           f"Last Name: {contact_form.contact_last_name.data}\n" \
+           f"Email: {contact_form.contact_email.data}\n" \
+           f"Phone: {contact_form.contact_phone.data}\n" \
+           f"Message: {contact_form.message.data}"
---
+++ get_the_job/static/js/app.js
@@ -0,0 +1,8 @@
+function getRandomNums() {
+    var randNum1 = Math.floor(Math.random() * 10);
+    var randNum2 = Math.floor(Math.random() * 10);
+    document.getElementById('random-number1').innerHTML = randNum1;
+    document.getElementById('input-rand-num1').value = randNum1;
+    document.getElementById('random-number2').innerHTML = randNum2;
+    document.getElementById('input-rand-num2').value = randNum2;
+}
